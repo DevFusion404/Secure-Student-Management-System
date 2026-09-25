20372db9f4b030403c4bdab5b4fbebc337413e29
PasanA <pasanamarasinghe2003@gmail.com>
2026-09-26 01:17:13 +0530

    XSS: Add xssEscape function for XSS protection: Encode untrusted values before rendering

--- database.php
+++ database.php
@@ -10,4 +10,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
+
+if (!function_exists('xssEscape')) {
+	// XSS: Encode untrusted values before rendering them in HTML text or attributes.
+	function xssEscape($value) {
+		return htmlspecialchars((string) $value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+	}
+}
 ?>
